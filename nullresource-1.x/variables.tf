1492737f1baf8aae7997358dbcb57021b62bd773
mahantidinesh7 <61232160+mahantidinesh7@users.noreply.github.com>
2022-10-27 15:23:40 +0530

    Create variables.tf

---
+++ nullresource-1.x/variables.tf
@@ -0,0 +1,10 @@
+#####################################################################
+##
+##      Created 11/18/18 by admin. for aws-single-instance
+##
+#####################################################################
+
+variable "greeting_message" {
+  type = string
+  description = "Greeting Message"
+}
